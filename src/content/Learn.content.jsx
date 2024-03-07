4a2603c6619e021af7d7af11441f4eb41691ab08
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-03-07 12:43:18 -0800

    redesign the flex

--- src/content/Learn.content.jsx
+++ src/content/Learn.content.jsx
@@ -6,57 +6,52 @@ const Learn = () => {
     <div className="main-flex">
       <Fragment>
         <div className="main-grid">
-          <div className="section">
-            <img src="/section-img/pngegg.png" alt="1" className="img" />
-            <div className="title">
-              <h2>Section 1: Beginner</h2>
-              <Link className="" to="/lesson/daily-phrase">
-                <button className="button-7">CONTINUE</button>
-              </Link>
+          <div className="section"><img src="/section-img/pngegg.png" alt="1" className="img" />
+            <div className="small-section">
+              <div className="title">
+                <h2>Section 1: Beginner</h2>
+              </div>
+              <Link to="/lesson/daily-phrase" className="button-7">CONTINUE</Link>
             </div>
           </div>
         </div>
         <div className="main-grid">
-          <div className="section">
-            <img src="/section-img/charmander.png" alt="section-1" className="img" />
-            <div className="title">
-              <h2>Section 2: PreIntermediate</h2>
-              <Link className="" to="/lesson/daily-phrase">
-                <button className="button-7">CONTINUE</button>
-              </Link>
+          <div className="section"><img src="/section-img/pokemon.png" alt="1" className="img" />
+            <div className="small-section">
+              <div className="title">
+                <h2>Section 2: Pre-Intermediate</h2>
+              </div>
+              <Link to="/lesson/daily-phrase" className="button-7">CONTINUE</Link>
             </div>
           </div>
         </div>
         <div className="main-grid">
-          <div className="section">
-            <img src="/section-img/pokemon.png" alt="section-1" className="img" />
-            <div className="title">
-              <h2>Section 3: Intermediate</h2>
-              <Link className="" to="/lesson/daily-phrase">
-                <button className="button-7">CONTINUE</button>
-              </Link>
+          <div className="section"><img src="/section-img/toroto.png" alt="1" className="img" />
+            <div className="small-section">
+              <div className="title">
+                <h2>Section 3: Intermediate</h2>
+              </div>
+              <Link to="/lesson/daily-phrase" className="button-7">CONTINUE</Link>
             </div>
           </div>
         </div>
         <div className="main-grid">
-          <div className="section">
-            <img src="/section-img/sonic.png" alt="section-1" className="img" />
-            <div className="title">
-              <h2>Section 4: Upper-Intermediate</h2>
-              <Link className="" to="/lesson/daily-phrase">
-                <button className="button-7">CONTINUE</button>
-              </Link>
+          <div className="section"><img src="/section-img/charmander.png" alt="1" className="img" />
+            <div className="small-section">
+              <div className="title">
+                <h2>Section 4: Upper-Intermediate</h2>
+              </div>
+              <Link to="/lesson/daily-phrase" className="button-7">CONTINUE</Link>
             </div>
           </div>
         </div>
         <div className="main-grid">
-          <div className="section">
-            <img src="/section-img/toroto.png" alt="section-1" className="img" />
-            <div className="title">
-              <h2>Section 5: Advance</h2>
-              <Link className="" to="/lesson/daily-phrase">
-                <button className="button-7">CONTINUE</button>
-              </Link>
+          <div className="section"><img src="/section-img/sonic.png" alt="1" className="img" />
+            <div className="small-section">
+              <div className="title">
+                <h2>Section 5: Advance</h2>
+              </div>
+              <Link to="/lesson/daily-phrase" className="button-7">CONTINUE</Link>
             </div>
           </div>
         </div>
