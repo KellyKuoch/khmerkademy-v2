c065cbeaf30e313932198aaee6aa2abbf337dc11
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-03-13 00:41:34 -0700

    added one more unit

--- src/units/Unit.units.jsx
+++ src/units/Unit.units.jsx
@@ -33,10 +33,7 @@ const Unit = () => {
               alt="logo"
               className="fish"
             />
-            
             </div>
-            
-            
           </div>
 
           <div className="insider-unit">
@@ -88,6 +85,33 @@ const Unit = () => {
             />
             <img
               src="https://placehold.co/600x400.png"
+              alt="logo"
+              className="fish"
+            />
+            </div>
+          </div>
+          <div className="insider-unit">
+            <div className="small-insider-unit">
+              <h2>Unit 4</h2>
+            </div>
+            <div className="small-insider-unit">
+            <img
+              src="https://placehold.co/600x400?text=Consonent"
+              alt="logo"
+              className="fish"
+            />
+            <img
+              src="https://placehold.co/600x400?text=Vowel"
+              alt="logo"
+              className="fish"
+            />
+            <img
+              src="https://placehold.co/600x400?text=Vocabulary"
+              alt="logo"
+              className="fish"
+            />
+            <img
+              src="https://placehold.co/600x400?text=Conversation"
               alt="logo"
               className="fish"
             />
