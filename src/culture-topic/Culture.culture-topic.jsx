1e676a2b1f82cb21d931ea1b5cf0b2b4058b771c
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-03-15 01:03:55 -0700

    redesign learn section

--- src/culture-topic/Culture.culture-topic.jsx
+++ src/culture-topic/Culture.culture-topic.jsx
@@ -1,19 +1,45 @@
-import { Outlet, Link } from "react-router-dom";
+
 import "./Culture.culture-topic.css"
 const Culture = () => {
   return (
-    <div className="fun-fact-style">
-      <h2>Fun Facts</h2>
-      <div className="small-topic-culture">
-        <img src="https://placehold.co/600x400.png" alt="topic1" className="culture-img"/>
-        <h3>Khmer New Year</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
-      <div className="small-topic-culture">
-        <img src="https://placehold.co/600x400.png" alt="topic1" className="culture-img"/>
-        <h3>Pchum Ben</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
+    <div className="articles">
+      <article>
+        <div className="article-wrapper">
+          <figure>
+            <img src="/culture-img/royal-palace.jpeg" alt="royalPalace"/>
+          </figure>
+          <div className="article-body">
+            <h2>Royal Palace</h2>
+            <p>
+              The Royal Palace is located in the city of Phnom Penh. The
+              establishment of the Royal Palace in Phnom Penh in 1866 is a
+              comparatively recent event in the history of the Khmer and
+              Cambodia.{" "}
+            </p>
+            {/* <a href="#" className="read-more">
+              Read more
+            </a> */}
+          </div>
+        </div>
+      </article>
+      <article>
+        <div className="article-wrapper">
+          <figure>
+            <img src="/culture-img/Angkor-Wat.jpg" alt="angkor"/>
+          </figure>
+          <div className="article-body">
+            <h2>Angkor Wat</h2>
+            <p>
+              Angkor Wat, located in Phnom Penh, Cambodia, is one of the largest
+              and most beautiful archaeological sites in the world. It is a
+              UNESCO World Heritage Site.
+            </p>
+            {/* <a href="#" className="read-more">
+              Read more
+            </a> */}
+          </div>
+        </div>
+      </article>
     </div>
   );
 };
