4b2eea79df917c55ca99008c97ae38a64ba209d1
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-03-21 22:56:44 -0700

    added a homepage placeholder

--- src/components/Homepage.component.jsx
+++ src/components/Homepage.component.jsx
@@ -1,21 +1,95 @@
 import "./Homepage.css";
 
-const Homepage = () => {
+
+
+export default function App() {
   return (
-    <div className="container-1">
-      <div className="slides slide1">
-        <img src="./img/angkor-wat.png" alt="pretty" />
-        <h2>Unlocking Cambodia's Potential, One Lesson at a Time</h2>
+    <div className="main-homepage">
+      <div className="top-container">
+        <div className="left-top">
+          <h2>Learn a language for free. Forever</h2>
+          <div className="button-row">
+            <button>Log In</button>
+            <button>Sign Up</button>
+          </div>
+        </div>
+        <img
+          className="image-class"
+          src="https://placehold.co/600x400.png"
+          alt="top-pic"
+        />
       </div>
-      <div className="slides slide2">
-        <h2>
-          Don't look any further, KhmerKademy is the place to learn a new
-          language.
-        </h2>
-        <img src="./img/pretty-girl.GIF" alt="pretty" />
+      <div className="middle-container">
+        <h2>Gamification poured into every lesson</h2>
+        <div className="lesson-preview">
+          <div className="lesson-container">
+            <img
+              className="image-small"
+              src="https://placehold.co/600x400.png"
+              alt="small-pic"
+            />
+            <h3>Personalized Learning</h3>
+          </div>
+          <div className="lesson-container">
+            <img
+              className="image-small"
+              src="https://placehold.co/600x400.png"
+              alt="small-pic"
+            />
+            <h3>Improve Quickly</h3>
+          </div>
+          <div className="lesson-container">
+            <img
+              className="image-small"
+              src="https://placehold.co/600x400.png"
+              alt="small-pic"
+            />
+            <h3>Stay Motivated</h3>
+          </div>
+          <div className="lesson-container">
+            <img
+              className="image-small"
+              src="https://placehold.co/600x400.png"
+              alt="small-pic"
+            />
+            <h3>New Lesson Every Week</h3>
+          </div>
+        </div>
+      </div>
+      <div className="footer-container">
+        <img
+          className="image-class"
+          src="https://placehold.co/600x400.png"
+          alt=""
+        />
+        <div className="decriptions">
+          <h2>Welcome to KhmerKademy</h2>
+          <p>Your new language learning journey starts here</p>
+          <button>Start Learning</button>
+        </div>
+      </div>
+      <div className="footer-container">
+        <div className="decriptions">
+          <h2>Welcome to KhmerKademy</h2>
+          <p>Your new language learning journey starts here</p>
+        </div>
+        <img
+          className="image-class"
+          src="https://placehold.co/600x400.png"
+          alt=""
+        />
+      </div>
+      <div className="footer-container">
+        <img
+          className="image-class"
+          src="https://placehold.co/600x400.png"
+          alt=""
+        />
+        <div className="decriptions">
+          <h2>Welcome to KhmerKademy</h2>
+          <p>Your new language learning journey starts here</p>
+        </div>
       </div>
     </div>
   );
-};
-
-export default Homepage;
+}
