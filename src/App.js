43270a48022f2e931b06f61576a44c1e75b04c9f
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-04-08 15:08:15 -0700

    added consonant and vowels

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Unit from "./units/Unit.units";
 import Culture from "./culture-topic/Culture.culture-topic";
 import Vocabulary from "./units/Vocabulary.unit";
 import Conversatoin from "./units/Conversation.unit";
+import Consonants from "./units/unit-1/Consonants";
+import Vowels from "./units/unit-1/Vowels";
 
 const App = () => {
   return (
@@ -24,6 +26,8 @@ const App = () => {
         <Route path="exercise/matching-pair" element={<MatchingPair />} />
         <Route path="lessons" element={<Learn />} />
         <Route path="learn/section-1" element={<Unit />} />
+        <Route path="unit/consonants" element={<Consonants />} />
+        <Route path="unit/vowels" element={<Vowels />} />
         <Route path="unit/vocabulary" element={<Vocabulary />} />
         <Route path="unit/conversation" element={<Conversatoin />} />
         <Route path="profile" element={<Profile />} />
