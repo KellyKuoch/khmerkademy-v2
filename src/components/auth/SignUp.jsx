c8a5774e0a5992302bd227c12297f6ca9e7d4cb6
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-04-21 15:46:22 -0700

    added sign in and sign up component

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,14 +1,23 @@
+import "./SignUp.css";
 import { useState } from "react";
 import { auth } from "../../firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const SignUp = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  //sign in
+  const [signInEmail, setSignInEmail] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
+
+  //sing up
+  const [signUpEmail, setSignUpEmail] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
   const signUp = (e) => {
     // todo: sign in
     e.preventDefault();
-    createUserWithEmailAndPassword(auth, email, password)
+    createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
       .then((userCredential) => {
         console.log(userCredential);
       })
@@ -16,24 +25,77 @@ const SignUp = () => {
         console.log(error);
       });
   };
+
+  const signIn = (e) => {
+    // todo: sign in
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, signInEmail, signInPassword)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="sign-in-container">
-      <form onSubmit={signUp}>
-        <h1>Create an account</h1>
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <input
-          type="password"
-          placeholder="Enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <button type="submit">Sign Up</button>
-      </form>
+    <div className="auth-container">
+      <div className="sign-in-container">
+        <form onSubmit={signUp}>
+          <div className="title">Create an account</div>
+          <div className="input-box underline">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={signUpEmail}
+              onChange={(e) => setSignUpEmail(e.target.value)}
+            ></input>
+            <div className="underline"></div>
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={signUpPassword}
+              onChange={(e) => setSignUpPassword(e.target.value)}
+            ></input>
+            <div className="underline"></div>
+          </div>
+          <div className="input-box button">
+            <button className="button-8" type="submit">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="sign-in-container">
+        <form onSubmit={signIn}>
+          <div className="title">Already have an account?</div>
+          <div className="input-box underline">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={signInEmail}
+              onChange={(e) => setSignInEmail(e.target.value)}
+            ></input>
+            <div className="underline"></div>
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={signInPassword}
+              onChange={(e) => setSignInPassword(e.target.value)}
+            ></input>
+            <div className="underline"></div>
+          </div>
+          <div className="input-box button">
+            <button className="button-8" type="submit">
+              Log In
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
